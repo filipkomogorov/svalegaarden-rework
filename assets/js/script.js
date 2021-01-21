a88bf2804d0c4d144419ab2bc8fb7c9531b9461a
Filip Komogorov <filipkomogorov@gmail.com>
2021-01-22 00:52:46 +0100

    updates on menu, events and about us

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,13 @@ document.querySelector('#hamburger-menu').addEventListener('click', function () 
     document.querySelector('#slide-menu').classList.toggle('navigation-show')
     document.querySelector('#hamburger-menu').classList.toggle('hamburger-animation')
 })
+
+document.querySelector('#arrow-right').addEventListener('click', function () {
+    document.querySelector('#event-wedding').classList.toggle('events-click-left')
+    document.querySelector('#event-confirmation').classList.toggle('events-click-right')
+})
+
+document.querySelector('#arrow-left').addEventListener('click', function () {
+    document.querySelector('#event-wedding').classList.toggle('events-click-left')
+    document.querySelector('#event-confirmation').classList.toggle('events-click-right')
+})
