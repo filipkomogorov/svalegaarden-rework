63df4f62f68637646471aa3a8e91b79f9809b08e
Filip Komogorov <filipkomogorov@gmail.com>
2021-01-21 01:00:11 +0100

    progress of rework - about and main page completed for mobile

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('#hamburger-menu').addEventListener('click', function () {
+    document.querySelector('#slide-menu').classList.toggle('navigation-show')
+    document.querySelector('#hamburger-menu').classList.toggle('hamburger-animation')
+})
